b47888f0fa432e6a023ed184e22fa94260f9d2d4
Drew Oldag <awoldag@uw.edu>
2023-03-30 12:22:54 -0700

    Added unit tests and refactored the hook for easier testing.

--- src/pre_commit_hooks/check_template_version.py
+++ src/pre_commit_hooks/check_template_version.py
@@ -15,14 +15,78 @@ from typing import Sequence
 
 import git
 import yaml
-from packaging.version import InvalidVersion, parse
+from packaging.version import InvalidVersion, Version, parse
 
 
-def check_version(template_url: str, copier_answers_file: str) -> int:
+class FriendlyException(Exception):
+    "Something went wrong, but we don't want to block committing."
+    pass
+
+
+def _does_file_exist(copier_answer_file: str) -> bool:
+    return os.path.isfile(copier_answer_file)
+
+
+def _get_template_version(copier_config: dict) -> Version:
+    try:
+        return parse(copier_config.get("_commit", None))
+    except (TypeError, InvalidVersion) as exc:
+        raise FriendlyException("Cannot parse version string") from exc
+
+
+def _get_template_path(copier_config: dict) -> str:
+    try:
+        template_url = copier_config.get("_src_path", None)
+        return template_url.replace("gh://", "https://github.com/")
+    except AttributeError as exc:
+        raise FriendlyException("Cannot return _src_path for copier answers") from exc
+
+
+def _get_latest_remote_version(template_url: str) -> Version:
+    try:
+        remote_tags = _retrieve_git_remote_tags(template_url)
+        return _parse_git_blob(remote_tags)
+    except (Exception, FriendlyException) as exc:
+        raise FriendlyException("Failed to get latest version of remote template") from exc
+
+
+def _retrieve_git_remote_tags(template_url: str) -> str:
+    try:
+        g = git.cmd.Git()
+        return g.ls_remote(template_url, sort="-v:refname", tags=True)
+    except Exception as exc:
+        raise FriendlyException("Failed to get remote tags") from exc
+
+
+def _parse_git_blob(git_ls_remote_blob: str) -> Version:
+    try:
+        return parse(git_ls_remote_blob.split("\n")[0].split("/")[-1])
+    except (AttributeError, InvalidVersion, IndexError) as exc:
+        raise FriendlyException("Parsing the results of git ls-remote failed") from exc
+
+
+def _compare_versions(local_template_version: Version, latest_remote_version: Version) -> None:
+    try:
+        if local_template_version < latest_remote_version:
+            print("A new version of your project template is available!")
+            print(
+                f"Your version ({local_template_version}) is older than the latest ({latest_remote_version})"
+            )
+            print("Run the following command to update your template: \033[91mcopier\033[0m")
+            print(
+                "For more information see the documentation: https://lincc-ppt.readthedocs.io/en/latest/source/update_project.html"
+            )
+        else:
+            return 0
+    except Exception as exc:
+        raise FriendlyException("Failed to compare verisons")
+
+
+def check_version(copier_answers_file: str) -> int:
     """The main method"""
 
     # If we can't find the file, we'll just return 0 and move on
-    if not os.path.isfile(copier_answers_file):
+    if not _does_file_exist(copier_answers_file):
         return 0
 
     # If we fail to load the copier answers file, return 0 and move on
@@ -32,44 +96,35 @@ def check_version(template_url: str, copier_answers_file: str) -> int:
         except yaml.YAMLError:
             return 0
 
-    # If the semantic version parsing fails, return 0
     try:
-        local_copier_version = parse(copier_config.get("_commit", None))
-    except InvalidVersion:
+        local_copier_version = _get_template_version(copier_config)
+    except FriendlyException:
         return 0
 
-    # There are bunch of things that could go wrong with git commands, so we'll
-    # use a very broad `Exception` class to return 0.
     try:
-        g = git.cmd.Git()
-        blob = g.ls_remote(template_url, sort="-v:refname", tags=True)
-    except Exception:
+        template_url = _get_template_path(copier_config)
+    except FriendlyException:
         return 0
 
-    # If the version parsing fails, or the splitting/indexing fails, return 0
     try:
-        latest_remote_version = parse(blob.split("\n")[0].split("/")[-1])
-    except (InvalidVersion, IndexError):
+        latest_remote_version = _get_latest_remote_version(template_url)
+    except FriendlyException:
         return 0
 
-    if local_copier_version < latest_remote_version:
-        print(f"Time to update your template version from {local_copier_version} -> {latest_remote_version}")
-        print("Run the following command to update your template.")
-        print("copier update")
-
-    return 0
+    try:
+        _compare_versions(local_copier_version, latest_remote_version)
+    except FriendlyException:
+        return 0
 
 
 def main(argv: Sequence[str] | None = None) -> int:
     """Parse input arguments and return results of `check_version`"""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--template-url",
-        default="https://github.com/lincc-frameworks/python-project-template",
-        help="The repository URL for the template",
-    )
 
+    # --copier-answers-file will default to .copier-answers.yml, but allows for
+    # passing different values in order to support projects that are composed of
+    # multiple templates.
     parser.add_argument(
         "--copier-answers-file",
         default=".copier-answers.yml",
@@ -79,7 +134,7 @@ def main(argv: Sequence[str] | None = None) -> int:
     args = parser.parse_args(argv)
 
     try:
-        return check_version(args.template_url, args.copier_answers_file)
+        return check_version(args.copier_answers_file)
     except Exception:
         return 0
 
--- tests/pre_commit_hooks/test_check_template_version.py
+++ tests/pre_commit_hooks/test_check_template_version.py
@@ -1,2 +1,139 @@
-def test_nothing():
-    assert True
+"""Unit tests for check_template_version pre-commit hook"""
+
+import pytest
+from packaging.version import InvalidVersion, Version
+
+from pre_commit_hooks import FriendlyException, check_template_version
+
+
+def test_copier_file_exist():
+    test_file = "./tests/pre_commit_hooks/.testing-copier-answers.yml"
+    assert check_template_version._does_file_exist(test_file)
+
+
+def test_copier_file_does_not_exist():
+    test_file = "./tests/pre_commit_hooks/.bogus_file_name.yml"
+    assert not check_template_version._does_file_exist(test_file)
+
+
+def test_get_template_version():
+    version_string = "v1.3.0"
+    copier_config = {"_commit": version_string}
+    test_version = Version(version_string)
+    template_version = check_template_version._get_template_version(copier_config)
+    assert test_version == template_version
+
+
+def test_get_template_version_no_commit():
+    version_string = "v1.3.0"
+    copier_config = {"_nothing": version_string}
+    with pytest.raises(FriendlyException) as exc:
+        _ = check_template_version._get_template_version(copier_config)
+        assert "Cannot parse version string" in str(exc.value)
+
+
+def test_get_template_version_invalid_version():
+    version_string = "bogus_version_id"
+    copier_config = {"_commit": version_string}
+    with pytest.raises(FriendlyException) as exc:
+        _ = check_template_version._get_template_version(copier_config)
+        assert "Cannot parse version string" in str(exc.value)
+
+
+def test_get_template_url():
+    template_url = "https://github.com/example/repo"
+    copier_config = {"_src_path": template_url}
+    extracted_url = check_template_version._get_template_path(copier_config)
+    assert extracted_url == template_url
+
+
+def test_get_template_url_no_src():
+    template_url = "https://github.com/example/repo"
+    copier_config = {"_nothing": template_url}
+    with pytest.raises(FriendlyException) as exc:
+        _ = check_template_version._get_template_path(copier_config)
+        assert "Cannot return _src_path" in str(exc.value)
+
+
+def test_get_template_url_with_str_replace():
+    template_url = "gh://example/repo"
+    expected_url = "https://github.com/example/repo"
+    copier_config = {"_src_path": template_url}
+    extracted_url = check_template_version._get_template_path(copier_config)
+    assert extracted_url == expected_url
+
+
+def test_parse_git_blog():
+    test_blob = "cc9babf3057ec04cdffc7d8b2edce212d589a255\trefs/tags/v1.3.1\nb2a26468a6ad00573b4c61948b6959a08a25938e\trefs/tags/v1.3.0\n341bf28af7e3b167e53a7643bc42ceb5db341ab5\trefs/tags/v1.2.1\nbd1a5522f324b77984873f517364c8ce59d2e44b\trefs/tags/v1.1.0\n5c4250154fd5ceddb8c75b25f8eb690c0914dc28\trefs/tags/v1.0.0\nebb241dacaefab8d57d424dabb463cbce9f4df88\trefs/tags/v0.0.6\n3968ff997296c1b5c1b5965579173cb91ae2afcf\trefs/tags/v0.0.5\n1f2e6e938ceeb4f5c0407a798edf8598b50123a7\trefs/tags/v0.0.4\n859bbfbfa354aafba9d0bc83c8d060211bade1a7\trefs/tags/v0.0.3\nb47caea55a20285fecf3a2f17e3eed3e5db0274a\trefs/tags/v0.0.2\n51b8601c413fab872d31d8f9cc78c8510c2e2e54\trefs/tags/v0.0.1\nfb6197d45acc6cc547b474ab6b878297b4641ff5\trefs/tags/1.2.0"
+    expected_version = Version("v1.3.1")
+    output_version = check_template_version._parse_git_blob(test_blob)
+    assert expected_version == output_version
+
+
+def test_parse_git_blob_none_blob():
+    test_blob = None
+    with pytest.raises(FriendlyException) as exc:
+        _ = check_template_version._parse_git_blob(test_blob)
+        assert "Parsing the results of git ls-remote failed" in str(exc.value)
+
+
+def test_parse_git_blob_empty_blob():
+    test_blob = ""
+    with pytest.raises(FriendlyException) as exc:
+        _ = check_template_version._parse_git_blob(test_blob)
+        assert "Parsing the results of git ls-remote failed" in str(exc.value)
+
+
+def test_parse_git_blog_no_tag():
+    test_blob = "cc9babf3057ec04cdffc7d8b2edce212d589a255\trefs/tags"
+    with pytest.raises(FriendlyException) as exc:
+        _ = check_template_version._parse_git_blob(test_blob)
+        assert "Parsing the results of git ls-remote failed" in str(exc.value)
+
+
+def test_get_latest_remote_version(mocker):
+    test_blob = "cc9babf3057ec04cdffc7d8b2edce212d589a255\trefs/tags/v1.3.1\nb2a26468a6ad00573b4c61948b6959a08a25938e\trefs/tags/v1.3.0\n341bf28af7e3b167e53a7643bc42ceb5db341ab5\trefs/tags/v1.2.1\nbd1a5522f324b77984873f517364c8ce59d2e44b\trefs/tags/v1.1.0\n5c4250154fd5ceddb8c75b25f8eb690c0914dc28\trefs/tags/v1.0.0\nebb241dacaefab8d57d424dabb463cbce9f4df88\trefs/tags/v0.0.6\n3968ff997296c1b5c1b5965579173cb91ae2afcf\trefs/tags/v0.0.5\n1f2e6e938ceeb4f5c0407a798edf8598b50123a7\trefs/tags/v0.0.4\n859bbfbfa354aafba9d0bc83c8d060211bade1a7\trefs/tags/v0.0.3\nb47caea55a20285fecf3a2f17e3eed3e5db0274a\trefs/tags/v0.0.2\n51b8601c413fab872d31d8f9cc78c8510c2e2e54\trefs/tags/v0.0.1\nfb6197d45acc6cc547b474ab6b878297b4641ff5\trefs/tags/1.2.0"
+    template_url = "https://github.com/example/repo"
+    mocker.patch("pre_commit_hooks.check_template_version._retrieve_git_remote_tags", return_value=test_blob)
+    expected_version = Version("v1.3.1")
+    output_version = check_template_version._get_latest_remote_version(template_url)
+    assert expected_version == output_version
+
+
+def test_get_latest_remote_version_with_parse_failure(mocker):
+    template_url = "https://github.com/example/repo"
+    mocker.patch(
+        "pre_commit_hooks.check_template_version._retrieve_git_remote_tags", side_effect=FriendlyException()
+    )
+    with pytest.raises(FriendlyException) as exc:
+        _ = check_template_version._get_latest_remote_version(template_url)
+        assert "Failed to get latest version" in str(exc.value)
+
+
+def test_compare_versions():
+    version_string = "v1.3.1"
+    local_version = Version(version_string)
+    remote_version = Version(version_string)
+    assert 0 == check_template_version._compare_versions(local_version, remote_version)
+
+
+def test_compare_versions_out_of_date(capfd):
+    local_version = Version("v1.2.0")
+    remote_version = Version("v1.3.1")
+    check_template_version._compare_versions(local_version, remote_version)
+    out, _ = capfd.readouterr()
+    assert "A new version" in str(out)
+
+
+def test_compare_versions_local_ahead():
+    local_version = Version("v1.4.0")
+    remote_version = Version("v1.3.0")
+    assert 0 == check_template_version._compare_versions(local_version, remote_version)
+
+
+def test_compare_versions_wrong_type():
+    version_string = "v1.3.1"
+    remote_version = Version(version_string)
+    with pytest.raises(FriendlyException) as exc:
+        check_template_version._compare_versions(version_string, remote_version)
+        assert "Failed to compare" in str(exc.value)
